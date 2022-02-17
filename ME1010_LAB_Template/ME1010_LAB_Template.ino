ff35bea810e594ab14fde821f167b50a2fd31c38
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2022-02-17 11:16:23 -0700

    Lab Template

---
+++ ME1010_LAB_Template/ME1010_LAB_Template.ino
@@ -0,0 +1,58 @@
+/****************************************************************
+Author Names: [REPLACE WITH BOTH OF YOUR NAMES!]
+Lab section:
+Team number:
+Date:  
+Sketch Description:  
+
+Button Usage: Up/Down    -  
+              Left/Right -  
+              Select     -  
+
+Pin Usage:    Pin type/number     Hardware 
+              ----------------    ----------------                   
+              
+*******************************************************************/
+ 
+/****************************
+ ** #defines and #includes **
+ ****************************/ 
+ 
+/***********************
+ ** Global Variables ***
+ ***********************/
+// *** Declare & Initialize Pins ***
+
+// *** Create Servo Objects ***
+  
+// *** Declare & Initialize Program Variables ***
+
+/********************
+ ** Setup Function **
+ ********************/  
+void setup(void){
+  // PUT YOUR SETUP CODE HERE, TO RUN ONCE:
+
+  // *** Initialize Serial Communication ***
+  
+  // *** Configure Digital Pins & Attach Servos ***
+  
+  // *** Take Initial Readings ***
+  
+  // *** Move Hardware to Desired Initial Positions ***
+  
+}// end setup() function
+
+/*******************
+ ** Loop Function **
+ *******************/
+void loop(void){
+  //PUT YOUR MAIN CODE HERE, TO RUN REPEATEDLY
+  
+} // end loop() function
+
+/****************************
+ ** User-Defined Functions **
+ ****************************/
+// create custom headers as necessary to clearly organize your sketch
+// e.g., Button functions, DC Motor functions, Servo functions, etc.
