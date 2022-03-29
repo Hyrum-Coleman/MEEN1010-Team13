9f7dde7c227fbe8c97f3051a7d22d9685ad083be
Hyrum Coleman <99374726+Hyrum-Coleman@users.noreply.github.com>
2022-03-29 16:25:42 -0600

    Update Demo2.m

--- Demo2/Demo2.m
+++ Demo2/Demo2.m
@@ -14,19 +14,28 @@ clear; clc; close all;
 % Initializing Varialbes
 linkageFilename = 'Robot12_LinkageData.xlsx';
 load L_vector.mat
-[thetaS, thetaL] = LinkageData(linkageFilename);
+load H_vector.mat
+[thetaS_exper, thetaL_exper] = LinkageData(linkageFilename);
 
 % Finding minimum value of params
-[optimal_params, paramsSSE] = fminsearch(@CompareLinkageData, [0, 0, 0], [], L, thetaS, thetaL);
+[optimal_params, paramsSSE] = fminsearch(@CompareLinkageData, [0, 0, 0], [], L, thetaS_exper, thetaL_exper);
 
 % Graphing the information
 LinkageData(linkageFilename);
 hold on
-ThetaLaunch(L, thetaS, optimal_params);
+ThetaLaunch(L, thetaS_exper, optimal_params);
 legend('experiment', 'theory', 'Location', 'southeast');
 graphTxt = sprintf("alpha = %.2f\nbeta = %.2f\nthetaL0 = %.2f\nSSE = %.4f", optimal_params(1), optimal_params(2), optimal_params(3), paramsSSE);
 text(20, 70, graphTxt);
 
+thetaL = [47, 54, 61, 68];
+% thetaL = [45, 51, 57, 63]
+
+thetaS = ThetaServo(H, thetaL, optimal_params);
+
+for i=1:length(thetaS)
+fprintf("Use a servo angle of %.2f deg to achieve a launch angle of %.2f deg\n", thetaS(i), thetaL(i));
+end
 
 %%
 % Linkage
